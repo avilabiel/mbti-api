d143e172ef25457e07014813a0298114d0db6143
Gabriel Ávila <avilabiel@gmail.com>
2023-01-29 18:45:27 -0300

    so now we enabled all CORS

--- src/externals/nest/bootstrap.ts
+++ src/externals/nest/bootstrap.ts
@@ -3,6 +3,7 @@ import Module from "./module";
 
 async function bootstrap() {
   const app = await NestFactory.create(Module);
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 
